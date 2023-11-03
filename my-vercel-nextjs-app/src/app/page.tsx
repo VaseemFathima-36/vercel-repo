97c4daab3d9fd66756c0c50dd05c91e1368d22a6
VaseemFathima-36 <MVaseem.Fathima@cognizant.com>
2023-11-03 15:37:37 +0530

    sample code added

---
+++ my-vercel-nextjs-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Component } from '../components/component/component'
+// import Image from 'next/image'
+
+export default function Home() {
+  return (
+<div>
+  <h1>HOME PAGE</h1>
+  <Component/>
+</div>
+  )
+}
